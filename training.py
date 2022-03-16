9e22825be95bf57f9cc0f6227060ba7b7155f020
Lakshay Karnwal <60047109+lakshaykarnwal@users.noreply.github.com>
2022-03-15 19:35:35 -0700

    minor fixes

--- training.py
+++ training.py
@@ -78,5 +78,5 @@ model.compile(loss='categorical_crossentropy',
 
 hist = model.fit(np.array(train_x), np.array(train_y),
           epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.model.h5', hist) #export the ML model
+model.save('chatbot_model.h5', hist) #export the ML model
 print("Done")
