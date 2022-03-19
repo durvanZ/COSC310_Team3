8c9dfb76e655d1686126daf2d675be80688534bf
durvanZ <durvan@student.ubc.ca>
2022-03-18 18:02:00 -0700

    chatbot_gui.py

--- chatbot_gui.py
+++ chatbot_gui.py
@@ -1,18 +1,40 @@
 from predict import *
 from tkinter import *
 
+empathy_response = ""
+cat_vids = "https://www.youtube.com/results?search_query=cat+videos"
+dank_memes = "https://www.reddit.com/r/dankmemes/"
+res = ""
 root = Tk()
 
 
 def send():
 
     message = e.get()
+    usermood = get_mood(message)
+    negative_vibes = usermood.get('neg')
+    positive_vibes = usermood.get('pos')
+    
     gui_output = "You: {0}".format(
         "*makes eye contact" if not message else message)
     txt.insert(END, "\n" + gui_output)
+    
     ints = predict_classes(message)
-    response_bot = "Bot: {0}".format(get_response(ints, intents))
-    txt.insert(END, "\n" + response_bot)
+    response_predict = get_response(ints, intents)
+
+    if (negative_vibes >= 0.6):
+        empathy_response = random.choice(["I am really sorry to hear that", "We're sorry about that...",
+                                         ":(( That's so unfortunate", "I understand your sentiment. Please allow me to help you, what do you need to know?"])
+        res = empathy_response
+        if (negative_vibes >= 0.7):
+            res += random.choice(
+                [f"Here are some cat videos: {cat_vids}", f"Here are some dank memes: {dank_memes}"])
+    elif (positive_vibes >= 0.7):
+        empathy_response = random.choice([":)", "Happy spring!!!", "I'm glad you're satistifed with my service",
+                                         "Fantastic!!!", "Awesome", "I love to hear that", "You fill my heart with joy :))"])+"\n"
+   
+    final_response = "Bot: {0}".format(empathy_response) + response_predict
+    txt.insert(END, "\n" + final_response)
     e.delete(0, END)
 
 
